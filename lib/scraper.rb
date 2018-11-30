8668b126d5b9737d9897958dbd371b8e7cc7f17e
Rose Brennan <brennan.m.rose@gmail.com>
2018-11-30 12:22:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,10 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
+<<<<<<< HEAD
     doc = Nokogiri::HTML(open(index_url))
+=======
+>>>>>>> 166f5dc6f3681905ce772ac24153740560ba2676
     binding.pry
   end
 
